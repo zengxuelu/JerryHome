30522dcab83df7a7667e462e2891485659026b9e
曾学炉 <30375016+zengxuelu@users.noreply.github.com>
2017-08-16 15:52:10 +0800

    Update jquery.waterfall.js

--- jquery.waterfall.js
+++ jquery.waterfall.js
@@ -13,7 +13,7 @@ $.fn.waterfall = function () {
   var boxWidth = $item.outerWidth();
   //一列可以摆多少
   var coloums = parseInt((pageWidth + 10) / (boxWidth + gap));
-  
+  //创建一个数组,存放每张图片的高度
   var array = [];
   
   //先摆第一列
@@ -22,6 +22,7 @@ $.fn.waterfall = function () {
   }
   
   $item.each(function () {
+    //找最小高和最小高度所在的位置(index)
     var min = array[0];
     var index = 0;
     for (var i = 0; i < array.length; i++) {
@@ -34,6 +35,7 @@ $.fn.waterfall = function () {
       top: min,
       left: index * (boxWidth + gap)
     });
+    //更新最小高度
     array[index] = min + $(this).outerHeight();
   })
 }
